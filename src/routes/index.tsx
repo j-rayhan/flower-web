082ff023efc67b8cc94c55b2b82a55146b0161e2
johir-rayhan <johir.rayhan01@gmail.com>
2021-10-20 18:55:57 +0600

    Order section done

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
+import videoUrl from '../assets/images/about-vid.mp4';
+import { icons } from '../utils/data';
 
 const Routes: React.FC = () => {
     return (
@@ -33,6 +35,68 @@ const Routes: React.FC = () => {
                     </a>
                 </div>
             </header>
+            {/* <!-- home section starts  --> */}
+
+            <section className="home" id="home">
+                <div className="content">
+                    <h3>fresh flowers</h3>
+                    <span> natural & beautiful flowers </span>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae laborum ut
+                        minus corrupti dolorum dolore assumenda iste voluptate dolorem pariatur.
+                    </p>
+                    <a href="#start" className="btn">
+                        shop now
+                    </a>
+                </div>
+            </section>
+
+            {/* <!-- about section  --> */}
+
+            <section className="about" id="about">
+                <h1 className="heading">
+                    <span> about </span> us{' '}
+                </h1>
+
+                <div className="row">
+                    <div className="video-container">
+                        <video src={videoUrl} loop autoPlay muted />
+                        <h3>best flower sellers</h3>
+                    </div>
+
+                    <div className="content">
+                        <h3>why choose us?</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem cumque sit
+                            nemo pariatur corporis perspiciatis aspernatur a ullam repudiandae autem
+                            asperiores quibusdam omnis commodi alias repellat illum, unde optio
+                            temporibus.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium ea
+                            est commodi incidunt magni quia molestias perspiciatis, unde repudiandae
+                            quidem.
+                        </p>
+                        <a href="#learn-more" className="btn">
+                            learn more
+                        </a>
+                    </div>
+                </div>
+            </section>
+
+            {/* <!-- icons section  --> */}
+
+            <section className="icons-container">
+                {icons.map((item) => (
+                    <div className="icons" key={item.key}>
+                        <img src={item.img} alt="" />
+                        <div className="info">
+                            <h3>{item.title}</h3>
+                            <span>{item.description}</span>
+                        </div>
+                    </div>
+                ))}
+            </section>
         </div>
     );
 };
--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,13 +1,39 @@
+import icons_1 from '../assets/images/icon-1.png';
+import icons_2 from '../assets/images/icon-2.png';
+import icons_3 from '../assets/images/icon-3.png';
+import icons_4 from '../assets/images/icon-4.png';
+
 const data = [
     {
         key: 1,
     },
 ];
 
-const data2 = [
+const icons = [
     {
-        key: 1,
+        key: '1',
+        img: icons_1,
+        title: 'free delivery',
+        description: 'on all orders',
+    },
+    {
+        key: '2',
+        img: icons_2,
+        title: '10 days returns',
+        description: 'moneyback guarantee',
+    },
+    {
+        key: '3',
+        img: icons_3,
+        title: 'offer & gifts',
+        description: 'on all orders',
+    },
+    {
+        key: '4',
+        img: icons_4,
+        title: 'secure paymens',
+        description: 'protected by paypal',
     },
 ];
 
-export { data, data2 };
+export { data, icons };
